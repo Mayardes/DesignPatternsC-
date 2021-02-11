9b0e6127a4598f3f88574b8fd72e5a833de89f26
mayardes <mayardesoliveira@gmail.com>
2021-02-10 22:03:08 -0400

    Added: InformationHiding

---
+++ InformationHiding/Car.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InformationHiding
+{
+    public class Car
+    {
+        //hiding information details about my car
+        // composition class
+
+        private string _brand { get; set;}
+        private Engine _engine { get; set; }
+        private Batery _batery { get; set; }
+
+        private void Ignition()
+        {
+            Console.WriteLine("Startup the car..");
+        } 
+
+        public string Model {get; set; }
+
+        public Car(string brand)
+        {
+            this._brand = brand;
+        }
+
+        public int FactoryYear { get {return 2021; } private set { } }
+
+
+        public int NumTires()
+        {
+            return 4;
+        }
+
+
+        public void ToFillUp()
+        {
+            Console.WriteLine("Filling up car");
+        }
+        public void TurnOn()
+        {
+            Console.WriteLine("Turning on car");
+            Ignition();
+        }
+        public void ToMove()
+        {
+            Console.WriteLine("Moving car");
+        }
+
+    }
+}
---
+++ InformationHiding/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace InformationHiding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var car = new Car("Fiat");
+
+            car.TurnOn();
+            car.ToFillUp();
+            car.ToMove();
+        }
+    }
+}
